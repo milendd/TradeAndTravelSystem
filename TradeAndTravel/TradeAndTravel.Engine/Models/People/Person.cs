79382d5ef93f6c9bee47af954a46b472807617f1
milendd <milen_d@abv.bg>
2019-06-20 15:46:13 +0300

    Add people

---
+++ TradeAndTravel/TradeAndTravel.Engine/Models/People/Person.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TradeAndTravel.Engine.Models.Items;
+using TradeAndTravel.Engine.Models.Locations;
+
+namespace TradeAndTravel.Engine.Models.People
+{
+    public class Person : WorldObject
+    {
+        private HashSet<Item> inventoryItems;
+
+        public Person(string name, Location location)
+            : base(name)
+        {
+            this.Location = location;
+            this.inventoryItems = new HashSet<Item>();
+        }
+        
+        public Location Location { get; protected set; }
+        
+        public void AddToInventory(Item item)
+        {
+            this.inventoryItems.Add(item);
+        }
+
+        public void RemoveFromInventory(Item item)
+        {
+            this.inventoryItems.Remove(item);
+        }
+
+        public List<Item> ListInventory()
+        {
+            var items = this.inventoryItems.ToList();
+            return items;
+        }
+    }
+}
---
+++ TradeAndTravel/TradeAndTravel.Engine/Models/People/Shopkeeper.cs
@@ -0,0 +1,24 @@
+﻿using TradeAndTravel.Engine.Interfaces;
+using TradeAndTravel.Engine.Models.Items;
+using TradeAndTravel.Engine.Models.Locations;
+
+namespace TradeAndTravel.Engine.Models.People
+{
+    public class Shopkeeper : Person, IShopkeeper
+    {
+        public Shopkeeper(string name, Location location)
+            : base(name, location)
+        {
+        }
+
+        public virtual int CalculateSellingPrice(Item item)
+        {
+            return item.Value;
+        }
+
+        public virtual int CalculateBuyingPrice(Item item)
+        {
+            return item.Value / 2;
+        }
+    }
+}
